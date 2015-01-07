4542278c026c5a8bcec663a11b3b8d4a63f6cad5
Daniel Grant <daniel.s.grant12@gmail.com>
2015-01-07 15:03:46 -0800

    Complete overhaul. Various improvements

--- assist-cl.rb
+++ assist-cl.rb
@@ -1,107 +1,77 @@
-#Test ruby script for getting information from websites
-#Goal: emulate assist.org
-#ask user for current school, desired school, and major and save the agreement as a .txt file
+#assist-cl.rb
 
-require 'net/http'
-require 'io/console'
 require 'nokogiri'
 require 'open-uri'
 
-school_keys = IO.readlines("school_keys.txt")
-school_values = IO.readlines("school_values.txt")
+welcome_page = Nokogiri::HTML(open('http://www.assist.org/web-assist/welcome.html'))
+
 schools = Hash.new
-for i in 0...school_keys.length do
-  school_keys[i] = school_keys[i][0, school_keys[i].length-2]
-  school_values[i] = school_values[i][0, school_values[i].length-1]
-  schools[school_keys[i]] = school_values[i]
+
+welcome_page.css('option').each do |school|
+  value = school['value']
+  if value != ""
+    schools[school.text] = value[0...value.length-5]
+  end
 end
-schools.each { |from, to| puts "#{from}: #{to}" }
+
+schools.each { |school, value| puts "#{school} => #{value}" }
 
 puts "Where do you go to school currently?"
 from = schools[gets.chomp]
-puts from
-puts "What school would you like to transfer to?"
-to = schools[gets.chomp]
-puts to
+puts "Where do you want to transfer to?"
+input = gets.chomp
+if input.include? 'UC'
+  input.gsub!('UC', 'University of California,')
+end
+if input.include? 'CSU'
+  input.gsub!('CSU', 'California State University,')
+end
+to = schools[input]
 puts "What is your major?"
-major = gets.chomp #Will be = majors[gets.chomp.to_sym] when majors.txt has been pulled from assist
+major_name = gets.chomp
 
-page = Nokogiri::HTML(open("http://web1.assist.org/cgi-bin/REPORT_2/Rep2.pl?aay=13-14&dora=ECON&oia=#{to}&ay=14-15&event=19&ria=#{to}&agreement=aa&sia=#{from}&ia=#{from}&dir=1&&sidebar=false&rinst=left&mver=2&kind=5&dt=2"))
+#Retrieve 'To' school's list of majors
+school_page = Nokogiri::HTML(open("http://web1.assist.org/web-assist/articulationAgreement.do?inst1=none&inst2=none&ia=#{from}&ay=14-15&oia=#{to}&dir=1"))
 
-agreement = page.css('PRE').text
-puts agreement
-=begin
-agreement.lines.each do |line|
-  puts 'This is happening'
-  puts remove_html(line)
-end
-=end
-
-=begin
-f = File.open("agreement.txt", 'w+') { |f| f.write(agreement) }
-lines = IO.readlines("agreement.txt")
-
-def no_symbols?(line)
-  !line.include? '<' and !line.include? '-' and !line.include? ' ' and !line.include? '(' and !line.include? ';'
+major_form = nil
+school_page.css('form').each do |form|
+  major_form = form if form["name"] == 'major'
 end
 
-lines_that_matter = []
-title = "From: #{from}, To: #{to}, Major: #{major} \n"
-lines_that_matter.push(title)
-
-#set the first line to '-'s to look pretty
-lines.each do |line|
-  if line[0..4] == '-----'
-    lines_that_matter.push(line)
-    break
+majors = Hash.new
+major_form.css('option').each do |major|
+  if major.text.include? major_name
+    if !major["value"].include? " "
+      majors[major.text] = major["value"]
+    else
+      value = major["value"].gsub(' ', '%20')
+      majors[major.text] = value
+    end
   end
 end
 
-i = 0
-lines.each do |line|
+major_list, i = [], 1
+majors.each do |major, value| 
+  puts "#{i}) #{major}"
+  major_list.push(value)
   i += 1
-  if line[0..3] == line[0..3].upcase and no_symbols?(line[0..3]) and line[0] != "\n" and line.include? '|'
-    lines_that_matter.push(line)
-    j = i
-    until lines[j][0..4] == '-----' do
-      lines_that_matter.push(lines[j]) unless i==1
-      j += 1
-    end
-    lines_that_matter.push(lines[j])
-  end
 end
 
-def bracket?(char)
-  char == '<' or char == '>'
-end
-=end
+puts "Please enter the number of the corresponding major above:"
+major = major_list[gets.chomp.to_i - 1]
 
-def remove_html(line)
-  if line.include? '<'
-    no_html = ''
-    dont_add = false
-    for i in 0...line.length do
-      dont_add = true if line[i] == '<'
-      no_html += line[i] unless bracket?(line[i]) or dont_add
-      dont_add = false if line[i] == '>'
-    end
-    return no_html
-  else
-    return line
-  end
-end
-=begin
-lines.each do |line|
-  line = remove_html(line)
-  puts line
+page = Nokogiri::HTML(open("http://web1.assist.org/cgi-bin/REPORT_2/Rep2.pl?aay=13-14&dora=#{major}&oia=#{to}&ay=14-15&event=19&ria=#{to}&agreement=aa&ia=#{from}&sia=#{from}&dir=1&&sidebar=false&rinst=left&mver=2&kind=5&dt=2"))
+
+agreement = page.css('body').text
+f = File.open("agreement.txt", 'w+') { |f| f.write(agreement) }
+
+#Reminder: Code below is just practice with scan and regex
+#Find a regex to match class title and use scan to get array of all classes
+
+def getCourseNums(agreement)
+  agreement.scan(/[A-Z]{3,8} [\d]{1,3}[A-Z]*/)
 end
 
-File.open("agreement.txt", "w") { |file| file.truncate(0) } #hopefully this clears the text file 
-
-f = File.open("agreement.txt", 'w') do |f|
-  lines.each do |line|
-    line = remove_html(line)
-    f.write(line)
-  end
-end
-=end
+puts agreement
+course_nums = getCourseNums(agreement)
+course_nums.each { |course| puts course }
